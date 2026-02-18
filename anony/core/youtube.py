348451d0462c0c079c51c40106cba77149185574
arclx724 <arclx724@gmail.com>
2026-02-19 00:06:41 +0530

    Update youtube.py

--- anony/core/youtube.py
+++ anony/core/youtube.py
@@ -6,6 +6,7 @@
 import os
 import re
 import yt_dlp
+import random
 import asyncio
 from py_yt import Playlist, VideosSearch
 from anony import logger
@@ -14,11 +15,18 @@ from anony.helpers import Track, utils
 class YouTube:
     def __init__(self):
         self.base = "https://www.youtube.com/watch?v="
+        self.cookie_dir = "anony/cookies"
         self.regex = re.compile(
             r"(https?://)?(www\.|m\.|music\.)?"
             r"(youtube\.com/(watch\?v=|shorts/|playlist\?list=)|youtu\.be/)"
             r"([A-Za-z0-9_-]{11}|PL[A-Za-z0-9_-]+)([&?][^\s]*)?"
         )
+
+    def get_cookies(self):
+        if not os.path.exists(self.cookie_dir):
+            return None
+        cookies = [f"{self.cookie_dir}/{f}" for f in os.listdir(self.cookie_dir) if f.endswith(".txt")]
+        return random.choice(cookies) if cookies else None
 
     def valid(self, url: str) -> bool:
         return bool(re.match(self.regex, url))
@@ -66,16 +74,21 @@ class YouTube:
 
     async def download(self, video_id: str, video: bool = False) -> str | None:
         url = self.base + video_id
+        cookie = self.get_cookies()
 
         ydl_opts = {
             "quiet": True,
             "no_warnings": True,
             "geo_bypass": True,
             "nocheckcertificate": True,
-            "username": "oauth2",
-            "password": "",
             "format": "bestaudio/best" if not video else "best[height<=?720]",
+            "extractor_args": {"youtube": ["player_client=ANDROID_TESTSUITE,WEB_EMBED,IOS"]},
+            "http_headers": {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+            }
         }
+        if cookie:
+            ydl_opts["cookiefile"] = cookie
 
         def _get_stream_url():
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
