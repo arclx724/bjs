10bff35b26a9d2bcfee99514bced19d4445de892
arclx724 <arclx724@gmail.com>
2026-02-18 17:46:14 +0530

    Update youtube.py

--- anony/core/youtube.py
+++ anony/core/youtube.py
@@ -110,6 +110,10 @@ class YouTube:
             "nocheckcertificate": True,
             "cookiefile": cookie,
             "format": "bestaudio/best" if not video else "best[height<=?720]",
+            "extractor_args": {"youtube": ["client=ANDROID_MUSIC,ANDROID,WEB"]},
+            "http_headers": {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+            }
         }
 
         def _get_stream_url():
